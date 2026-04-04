98e44d4f0da73abc570326fc3f66650ad0b5a84e
dllosmartapps <dllo.smartapps@gmail.com>
2026-04-04 14:47:22 -0500

    Update scraper.js

--- modules/scraper.js
+++ modules/scraper.js
@@ -3,21 +3,15 @@ import fetch from "node-fetch";
 const SERP_API_KEY = process.env.SERP_API_KEY;
 
 export async function buscarConvocatorias(query = "") {
-
-  const q = `convocatorias proyectos Colombia site:.gov OR site:.org ${query}`;
-
-  const url = `https://serpapi.com/search.json?q=${encodeURIComponent(q)}&api_key=${SERP_API_KEY}`;
+  const url = `https://serpapi.com/search.json?q=convocatorias+${query}&api_key=${SERP_API_KEY}`;
 
   const r = await fetch(url);
-  const data = await r.json();
+  const d = await r.json();
 
-  return data.organic_results?.slice(0, 5).map(x => ({
+  return d.organic_results?.slice(0, 3).map(x => ({
     nombre: x.title,
     link: x.link,
     descripcion: x.snippet,
-    sector: "social",
-    ods: ["4"],
-    pais: "Colombia",
-    monto: 50000000
+    sector: "social"
   })) || [];
 }
