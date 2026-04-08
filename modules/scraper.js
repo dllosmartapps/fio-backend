9efd65a3e699071ac67099a6f8163c7d6873e3af
dllosmartapps <dllo.smartapps@gmail.com>
2026-04-08 15:29:53 -0500

    Update scraper.js

--- modules/scraper.js
+++ modules/scraper.js
@@ -3,15 +3,21 @@ import fetch from "node-fetch";
 const SERP_API_KEY = process.env.SERP_API_KEY;
 
 export async function buscarConvocatorias(query = "") {
-  const url = `https://serpapi.com/search.json?q=convocatorias+${query}&api_key=${SERP_API_KEY}`;
+  try {
+    const url = `https://serpapi.com/search.json?q=convocatorias+${query}&api_key=${SERP_API_KEY}`;
 
-  const r = await fetch(url);
-  const d = await r.json();
+    const r = await fetch(url);
+    const d = await r.json();
 
-  return d.organic_results?.slice(0, 3).map(x => ({
-    nombre: x.title,
-    link: x.link,
-    descripcion: x.snippet,
-    sector: "social"
-  })) || [];
+    return d.organic_results?.slice(0, 3).map(x => ({
+      nombre: x.title,
+      link: x.link,
+      descripcion: x.snippet,
+      sector: "social"
+    })) || [];
+
+  } catch (error) {
+    console.error("ERROR SCRAPER:", error.message);
+    return [];
+  }
 }
